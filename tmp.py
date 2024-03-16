4c9b5a4105c5c661c02f1c1b6c3e0eb56972ea6c
polarpolar914 <kdw2003914@naver.com>
2024-03-16 14:52:16 +0900

    a

--- tmp.py
+++ tmp.py
@@ -6,14 +6,19 @@ jpype.startJVM(jpype.getDefaultJVMPath(), '-Xmx4096m')
 read = open('Embedding/output.txt', 'r', encoding='utf-8')
 text = read.read()
 read.close()
+text = list(text.split('\n'))
 
-# 형태소 분석
-kkma = Kkma()
-sentences = kkma.sentences(text)
-for idx, sentence in enumerate(sentences):
-    sentences[idx] = kkma.morphs(sentence)
+def process(text_list, i):
+    # 형태소 분석
+    kkma = Kkma()
+    sentences = kkma.sentences('\n'.join(text_list))
+    for idx, sentence in enumerate(sentences):
+        sentences[idx] = kkma.morphs(sentence)
 
-# 결과를 파일로 저장
-with open('morphs_output.txt', 'w', encoding='utf-8') as f:
-    for morphs in sentences:
-        f.write(' '.join(morphs) + '\n')
+    # 결과를 파일로 저장
+    with open('morphs_output'+str(i)+'.txt', 'w', encoding='utf-8') as f:
+        for morphs in sentences:
+            f.write(' '.join(morphs) + '\n')
+for i in range(0,len(text),100):
+    process(text[i:i+100],i)
+
