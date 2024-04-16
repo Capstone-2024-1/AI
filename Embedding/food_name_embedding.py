19daf63ca37097dedd317f8f7a189b2480f5897b
DONGWOO KIM <66837024+polarpolar914@users.noreply.github.com>
2024-04-16 20:07:08 +0900

    feat: 음식명 형태소 재배치(#5)

--- Embedding/food_name_embedding.py
+++ Embedding/food_name_embedding.py
@@ -3,19 +3,15 @@ from konlpy.tag import Okt
 import asyncio
 
 async def get_embedding_vector(query):
-    # 형태소 분석기 인스턴스 생성
     okt = Okt()
 
-    # query에서 명사만 추출하고 순서를 뒤집은 뒤 원래 query를 추가
     processed_query = ''.join(okt.nouns(query)[::-1]) + query
 
-    # 임베딩 모델 초기화
     embeddings = HuggingFaceEmbeddings(
         model_name="jhgan/ko-sroberta-multitask",
         encode_kwargs={'normalize_embeddings': True}
     )
 
-    # 비동기로 쿼리 임베딩
     embedding_vector = await embeddings.aembed_query(processed_query)
 
     return embedding_vector
