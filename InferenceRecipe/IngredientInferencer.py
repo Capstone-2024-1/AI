db848de4804a8da1647d99fb8954bcc6c50d6058
DONGWOO KIM <66837024+polarpolar914@users.noreply.github.com>
2024-05-17 16:40:56 +0900

    feat: 음식명과 파인튜닝된 Davinci모델로 식재료 추론(#8)

---
+++ InferenceRecipe/IngredientInferencer.py
@@ -0,0 +1,42 @@
+import asyncio
+
+from openai import OpenAI
+from dotenv import load_dotenv
+from PromptProducer import PromptProducer
+import os
+class IngredientInferencer:
+    def __init__(self):
+        load_dotenv()
+        self.model = os.getenv('FINE_TUNED_MODEL_ID')
+        self.api_key = os.getenv('OPENAI_API_KEY')
+        if not self.api_key:
+            raise ValueError("API key not found. Make sure it's set in the .env file.")
+        self.client = OpenAI(api_key=self.api_key)
+        self.tag_order = ['Abalone', 'Crab', 'Mussel', 'Other crustaceans', 'Oyster', 'Shrimp', 'Other shellfish', 'Butter', 'Cheese', 'Milk', 'Soy milk', 'Other dairy products', 'Eggs', 'Mackerel', 'Other fish', 'Other mollusks', 'Other seafood', 'Chilly', 'Cucumber', 'Tomato', 'Other fruiting vegetables', 'Apple', 'Banana', 'Kiwi', 'Mango', 'Peach', 'Other fruits', 'Barley', 'Beans', 'Buckwheat', 'Corn', 'Rice', 'Wheat', 'Other grains', 'Chives', 'Garlic', 'Green onion', 'Onion', 'Other herbage crop', 'Beef', 'Chicken', 'Duck', 'Lamb', 'Pork', 'Horse meat', 'Almond', 'Hazelnut', 'Peanut', 'Pinenuts', 'Pistachio', 'Walnut', 'Other nuts', 'Potato', 'Radish', 'Sweet potato', 'wild chive', 'Other root vegetables', 'Ginger', 'Honey', 'Pepper', 'Other seasonings']
+        self.prompt_producer = PromptProducer()
+
+    def clean_inference_result(self, input_str: str):
+        lower_to_original = {item.lower(): item for item in self.tag_order}
+
+        lower_input = input_str.lower()
+
+        included_strings = []
+
+        for lower_item, original_item in lower_to_original.items():
+            if lower_item in lower_input:
+                included_strings.append(original_item)
+
+        return included_strings
+
+    def infer(self, food_name):
+        prompt = asyncio.run(self.prompt_producer.produce(food_name))
+        completion = self.client.completions.create(
+            model=self.model,
+            prompt=prompt,
+            max_tokens=100,
+            seed=2061316412,
+            temperature=0
+        )
+        result = self.clean_inference_result(completion.choices[0].text)
+        print(completion.choices[0])
+        return result
