4f2ec261ccd68c5af4d19bbc78cabbb5652c255c
DONGWOO KIM <66837024+polarpolar914@users.noreply.github.com>
2024-05-20 23:49:51 +0900

    feat: 추론 및 검색결과 로깅 및 디스코드 알림, unvicon로그 파일로 저장

--- Backend/fastapi_services.py
+++ Backend/fastapi_services.py
@@ -3,18 +3,21 @@ from konlpy.tag import Okt
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from InferenceRecipe import IngredientInferencer
+from Logging.ResultLogger import ResultLogger
 
 embeddings = HuggingFaceEmbeddings(
     model_name="jhgan/ko-sroberta-multitask", encode_kwargs={'normalize_embeddings': True}
 )
 
-ingredient_inferencer = IngredientInferencer.IngredientInferencer()
+logger = ResultLogger()
+ingredient_inferencer = IngredientInferencer.IngredientInferencer(logger)
 
 async def load_faiss_cpu_db(db_index):
     db = FAISS.load_local(db_index, embeddings, allow_dangerous_deserialization=True)
     return db
 
 async def search_faiss_cpu_db(query, db, k=5):
+    original_query = query
     query = ''.join(Okt().nouns(query)[::-1]) + query
     embedding_vector = await embeddings.aembed_query(query)
 
@@ -23,12 +26,13 @@ async def search_faiss_cpu_db(query, db, k=5):
     score = docs_and_scores[0][1]
     is_ambiguous = False
     is_food = True
-    if score > 0.10:
+    if score > 0.08:
         is_ambiguous = True
     if score > 0.50:
         is_food = False
     ingredients = docs_and_scores[0][0].metadata['ingredients']
     ingredients = ingredients.replace('\b', '').replace('\n', '').replace('\t', '').strip()
+    logger.log(original_query + " : " + str(ingredients) + "\n" + str(docs_and_scores[0]) + "\n", False)
     return ingredients, is_ambiguous, is_food
 
 async def infer_ingredients(koreanName):
