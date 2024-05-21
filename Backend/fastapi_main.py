f30771d0f8d0f3b5de27bd122ff1a1ca5f0d3201
DONGWOO KIM <66837024+polarpolar914@users.noreply.github.com>
2024-05-21 10:16:24 +0900

    fix: fastapi관련 deprecated된 부분 수정(#7)

--- Backend/fastapi_main.py
+++ Backend/fastapi_main.py
@@ -3,30 +3,34 @@ from fastapi import FastAPI, Query
 from fastapi_services import load_faiss_cpu_db, search_faiss_cpu_db, infer_ingredients
 from fastapi_models import SearchResponse, InferenceResponse
 
-
 app = FastAPI()
 db = None
 
-@app.on_event("startup")
 async def startup_event():
     global db
     db = await load_faiss_cpu_db('../BuildingDB/SAFEAT_ASYNC_DB_INDEX')
 
+
+app.add_event_handler("startup", startup_event)
+
 @app.get("/search", response_model=SearchResponse)
-async def search(koreanName: str = Query(..., example="김치찌개")):
+async def search(koreanName: str = Query(..., examples=["김치찌개", "제육볶음"])):
     ingredients, is_ambiguous, is_food = await search_faiss_cpu_db(koreanName, db)
     ingredients = ingredients.replace("[", "").replace("]", "").replace("'", "").split(",")
-    
+
     for i in range(len(ingredients)):
         ingredients[i] = ingredients[i].strip()
     ingredient_responses = [{"englishName": ingredient} for ingredient in ingredients]
-    return SearchResponse(koreanName=koreanName, ingredients=ingredient_responses, isAmbiguous=is_ambiguous, isFood=is_food)
+    return SearchResponse(koreanName=koreanName, ingredients=ingredient_responses, isAmbiguous=is_ambiguous,
+                          isFood=is_food)
+
 
 @app.get("/inference", response_model=InferenceResponse)
-async def inference(koreanName: str = Query(..., example="삼겹살김치찌개")):
+async def inference(koreanName: str = Query(..., examples=["삼겹살김치찌개", "오징어볶음"])):
     ingredients = await infer_ingredients(koreanName)
     ingredient_responses = [{"englishName": ingredient} for ingredient in ingredients]
     return InferenceResponse(koreanName=koreanName, ingredients=ingredient_responses)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
