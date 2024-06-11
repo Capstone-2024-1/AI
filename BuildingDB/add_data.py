b2fd707826f4c358422b12df3d54753363262a85
DONGWOO KIM <66837024+polarpolar914@users.noreply.github.com>
2024-06-11 17:15:30 +0900

    feat: 재료 카테고리 검증로직 추가 (#34)

--- BuildingDB/add_data.py
+++ BuildingDB/add_data.py
@@ -1,5 +1,20 @@
 import asyncio
 from konlpy.tag import Okt
+ingredients = ['Fruits', 'Nuts', 'Soy milk', 'Vegetables', 'Meat', 'Grain', 'Eggs', 'Seafood', 'Seasonings', 'Dairy', 'Apple', 'Kiwi', 'Peach', 'Banana', 'Mango', 'Other fruits', 'Walnut', 'Almond', 'Pistachio', 'Hazelnut', 'Pinenuts', 'Peanut', 'Other nuts', 'herbage crop', 'Root Vegetables', 'Fruiting Vegetables', 'Onion', 'Garlic', 'Green onion', 'Chives', 'Other herbage crop', 'Potato', 'Sweet potato', 'Radish', 'wild chive', 'Other root vegetables', 'Cucumber', 'Chilly', 'Tomato', 'Other Fruiting Vegetables', 'Rice', 'Wheat', 'Barley', 'Corn', 'Buckwheat', 'Beans', 'Other Grains', 'Beef', 'Pork', 'Poultry', 'Lamb', 'Horse Meat', 'Chicken', 'Duck', 'fish', 'Mackerel', 'Other Fish', 'Other Mollusks', 'Crustaceans', 'Shrimp', 'Crab', 'Other Crustaceans', 'Shellfish', 'Abalone', 'Oyster', 'Mussel', 'Other Shellfish', 'Other seafood', 'Pepper', 'Ginger', 'Honey', 'Asafoetida', 'Other seasonings', 'Milk', 'Cheese', 'Butter', 'Other dairy products']
+ingredients = [i.lower() for i in ingredients]
+
+async def check_ingredient(ingredient):
+    ingredient_list = eval(ingredient)
+    ingredient_list = [i.lower() for i in ingredient_list]
+    check = True
+    invalid_list = []
+    for i in ingredient_list:
+        if i not in ingredients:
+            check = False
+            invalid_list.append(i)
+    if invalid_list: print(invalid_list)
+    return check
+
 async def build_faiss_cpu_db():
     import csv
     reader1 = csv.reader(open('../Data_Cleaning/10000recipe/Tagged_Ingredients.csv', 'r', encoding='utf-8'))
@@ -20,14 +35,20 @@ async def build_faiss_cpu_db():
             continue
         page_content = ''.join(Okt().nouns(row[0])[::-1]) + row[0]
         metadata = {'name': row[0], 'food_name': row[1], 'ingredients': row[3]}
-        Documents.append(Document(page_content, metadata))
+        if await check_ingredient(row[3]):
+            Documents.append(Document(page_content, metadata))
+        else:
+            print(f"Error: {row[0]}")
 
     for idx, row in enumerate(reader2):
         if idx == 0:
             continue
         page_content = ''.join(Okt().nouns(row[0])[::-1]) + row[0]
         metadata = {'name': row[0], 'food_name': row[1], 'ingredients': row[3]}
-        Documents.append(Document(page_content, metadata))
+        if await check_ingredient(row[3]):
+            Documents.append(Document(page_content, metadata))
+        else:
+            print(f"Error: {row[0]}")
 
     with open('additional.txt', 'r', encoding='utf-8') as f:
         reader = f.readlines()
@@ -38,7 +59,11 @@ async def build_faiss_cpu_db():
             ingredients = row[first_comma + 1:].strip()
             page_content = ''.join(Okt().nouns(food_name)[::-1]) + food_name
             metadata = {'name': food_name, 'food_name': food_name, 'ingredients': ingredients}
-            Documents.append(Document(page_content, metadata))
+            if await check_ingredient(ingredients):
+                Documents.append(Document(page_content, metadata))
+            else:
+                print(f"Error: {food_name}")
+
 
     from langchain_text_splitters import CharacterTextSplitter
 
